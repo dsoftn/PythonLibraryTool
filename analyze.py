64ce4fc14f5bc9f4eaf0b2acd699f46b5d731d9f
dsoftn <dsoftn@gmail.com>
2023-03-11 21:10:00 +0100

    Added TxtBoxPrinter class, and Roboto Mono font.

--- analyze.py
+++ analyze.py
@@ -119,5 +119,5 @@ class IspitajObjekat():
 
 class do_it():
     def __init__(self):
-        import PyQt5
-        i = IspitajObjekat(PyQt5)
+        import main_cls
+        i = IspitajObjekat(main_cls.TxtBoxPrinter.print_text)
--- main_cls.py
+++ main_cls.py
@@ -53,11 +53,56 @@ class Analyzer(QtWidgets.QMainWindow):
         self.ui.btn_nav_left.clicked.connect(self.btn_nav_left_click)
         self.ui.btn_nav_right.clicked.connect(self.btn_nav_right_click)
         self.ui.btn_nav_end.clicked.connect(self.btn_nav_end_click)
+        self.ui.btn_net_doc.clicked.connect(self.btn_net_doc_click)
         # Update tree
         self.update_tree()
         self.update_navigation_buttons()
         # Show window
         self.show()
+
+
+    def btn_net_doc_click(self):
+        """TEST"""
+        box = TxtBoxPrinter(self.ui.txt_info)
+        box.print_text(formating_flags="cls")
+        box.print_text("Proba da vidimo kako radi sa default podesavanjima")
+        box.print_text("Ovo je velicina 24, boja crvena, sve GLOBAL", "@size=24, @color=red")
+        box.print_text("Boja je plava za ovu liniju", "color=blue")
+        box.print_text("Boja je zelena i velicina 14 za ovu liniju", "color=green, size=14")
+        box.print_text("Nema Flag, vraca se na GLOBAL", "")
+        box.print_text("Na pocetak dokumenta", "move=start")
+        box.print_text("Font Roboto Mono Boja tamno plava", "font=Roboto Mono, color= darkblue")
+        box.print_text("Font Blackadder ITC Boja tamno plava", "font=Blackadder ITC, color= darkblue")
+        box.print_text("Na kraj dokumenta sa Roboto Mono fontom", "move=end, @font=Courier, @size=12, @color=black")
+        box.print_text("for i in result:")
+        box.print_text("    ovo je sa tab-om")
+        box.print_text("    ovo je sa razmakom")
+        box.print_text("    iiiiiiiiii 10xi")
+        box.print_text("    MMMMMMMMMM 10xM")
+        box.print_text("Na kraj dokumenta sa Courier fontom", "move=end, @font=Courier, @size=12, @color=black")
+        box.print_text("for i in result:")
+        box.print_text("    ovo je sa tab-om")
+        box.print_text("    ovo je sa razmakom")
+        box.print_text("    iiiiiiiiii 10xi")
+        box.print_text("    MMMMMMMMMM 10xM")
+        box.print_text("Na kraj dokumenta sa System Fixed fontom", "move=end, @font=fixed, @size=12, @color=darkblue")
+        box.print_text("for i in result:", "@bc=black, @fc=yellow")
+        box.print_text("    ovo je sa tab-om","size=16")
+        box.print_text("    ovo je sa razmakom","size = 8")
+        box.print_text("    iiiiiiiiii 10xi","size=10")
+        box.print_text("    MMMMMMMMMM 10xM")
+        box.print_text("", "@background color=white")
+        box.print_text("Crni deo ", "color=black, @size-20, @font=Verdana, n=0")
+        box.print_text("pa onda crveni ", "c=red, n=false")
+        box.print_text("BOLD ", "bold=yes, new line = NO")
+        box.print_text("plavo na kraju", "color=darkblue, underline=true")
+        box.print_text("HEADER", "size = 60, color=darkgreen, font = Elephant, move=top")
+
+        if box.flags_has_errors():
+            box.print_text("Errors:", "size=20, color=black")
+        result = box.get_flag_error_list()
+        for i in box.get_flag_error_list():
+            box.print_text(f"Flag: {i[0]}   Error: {i[1]}", "size=16, color=Darkred")
 
     def add_info_box_page(self, page: str = ""):
         """Adds an Info box page.
@@ -746,9 +791,11 @@ class Analyzer(QtWidgets.QMainWindow):
         char_format = QtGui.QTextCharFormat()
         color = QtGui.QColor()
         font = QtGui.QFont()
-        char_format = self.info_char_format
-        color = self.info_color
-        font = self.info_font
+        for i in range(1, len(commands)):
+            if commands[i][0] == "!" or commands[i][0] == "!!" or commands[i][0] == "!!!":
+                char_format = self.info_char_format
+                color = self.info_color
+                font = self.info_font
         no_new_line = False
         cursor_freeze = False
         cursor = self.ui.txt_info.textCursor()
@@ -1733,6 +1780,292 @@ class Database():
         self.conn.commit()
 
 
+class TxtBoxPrinter():
+    def __init__(self, QtWidgets_QTextEdit_object: object, use_txt_box_printer_formating: object = None):
+        """Prints the text in the Text Box using method TxtBoxPrinter.print_text
+        Default behavior: Prints one line of text and moves to a new line, placing the cursor at the end of the printout.
+        Args:
+            QtWidgets_QTextEdit_object (QTextEdit): The object in which the text is printed
+            use_txt_box_writer_formating (TxtBoxPrinter)(optional): TxtBoxPrinter object from which text formatting will be downloaded
+        """
+        # QTextEdit object to print in
+        self.box = QtWidgets_QTextEdit_object
+        # Define global formating objects
+        self.global_font = QtGui.QFont()
+        self.global_color = QtGui.QColor()
+        self.global_text_char_format = QtGui.QTextCharFormat()
+        self.cursor = QtWidgets_QTextEdit_object.textCursor()
+        self.no_new_line = False
+        self.errors = []
+        # Get formating
+        if use_txt_box_printer_formating is not None:
+            self._setup_global_formating(use_txt_box_printer_formating)
+        # Load Roboto Mono Font
+        roboto_mono_font_idx =  QtGui.QFontDatabase.addApplicationFont("RobotoMono-Regular.ttf")
+        result = QtGui.QFontDatabase.applicationFontFamilies(roboto_mono_font_idx)
+        if len(result) > 0:
+            self.roboto_mono_font = result[0]
+        else:
+            self.roboto_mono_font = ""
+
+    def _setup_global_formating(self, self_class: object):
+        self.global_font = self_class.get_font()
+        self.global_color = self_class.get_color()
+        self.global_text_char_format = self_class.get_text_char_format()
+
+    def get_font(self) -> object:
+        """Returns current QFont object
+        """
+        return self.global_font
+
+    def get_color(self) -> object:
+        """Returns current QColor object
+        """
+        return self.global_color
+
+    def get_text_char_format(self) -> object:
+        """Returns current QTextCharFormat object
+        """
+        return self.global_text_char_format
+
+    def print_text(self, text_to_print: str = "`None|Ignore`", formating_flags: str = ""):
+        """Prints the text in the Text Box.
+        Default behavior: Prints one line of text and moves to a new line, placing the cursor at the end of the printout.
+        Args:
+            QtWidgets_QTextEdit_object (QTextEdit): The object in which the text is printed
+            text_to_print (str)(optional): Text to be printed
+            formating_flags (str)(optional): Formating flags (font, color, etc.)
+            use_txt_box_writer_formating (TxtBoxPrinter)(optional): TxtBoxPrinter object from which text formatting will be downloaded
+        Flags (Commands):
+            "cls": ["cls", "clear"]: Clear content of text box (No value needed)
+            "color": ["color", "fc", "c", "foreground", "foreground_color", "fore_color"]: Sets the foreground text color (String)
+            "background": ["background", "background_color", "back_color", "bc", "background color"]: Sets the background text color (String)
+            "font_size": ["font_size", "size", "fs", "font size"]: Sets font size (Integer)
+            "font_name": ["font_name", "fn", "font name", "font"]: Sets font name (String)
+            "font_bold": ["font_bold", "fb", "font bold", "bold"]: Sets font to bold (True/False)
+            "font_underline": ["font_underline", "fu", "font underline", "underline", "font_under", "font under", "under"]: Sets font to underline (True/False)
+            "new_line": ["new_line", "nl", "new_l", "n", "new line"]: Default is 'True', if 'False' cursor will not go in new line
+            "Move": ["move", "position", "@move", "@position"]: Move cursor in beginning or end of text (Beginning/End)
+        """
+        # Make variables for text and flags
+        txt = text_to_print
+        self.flags = formating_flags
+        # Create a dictionary of synonyms for commands and values
+        comm_syn = {
+            "cls": ["cls", "clear"],
+            "color": ["color", "fc", "c", "foreground", "foreground_color", "fore_color", "fore"],
+            "@color": ["@color", "@fc", "@c", "@foreground", "@foreground_color", "@fore_color", "@fore"],
+            "background": ["background", "background_color", "back_color", "bc", "background color", "back"],
+            "@background": ["@background", "@background_color", "@back_color", "@bc", "@background color", "@back"],
+            "font_size": ["font_size", "size", "fs", "font size"],
+            "@font_size": ["@font_size", "@size", "@fs", "@font size"],
+            "font_name": ["font_name", "fn", "font name", "font"],
+            "@font_name": ["@font_name", "@fn", "@font name", "@font"],
+            "font_bold": ["font_bold", "fb", "font bold", "bold"],
+            "@font_bold": ["@font_bold", "@fb", "@font bold", "@bold"],
+            "font_underline": ["font_underline", "fu", "font underline", "underline", "font_under", "font under", "under"],
+            "@font_underline": ["@font_underline", "@fu", "@font underline", "@underline", "@font_under", "@font under", "@under"],
+            "new_line": ["new_line", "nl", "new_l", "n", "new line"],
+            "@new_line": ["@new_line", "@nl", "@new_l", "@n", "@new line"],
+            "Move": ["move", "position", "@move", "@position"]
+        }
+        val_syn = {
+            "True": ["true", "1", "yes", "ok"],
+            "False": ["false", "0", "no"],
+            "Start": ["start", "beginning", "begining", "in start", "in beginning", "in begining", "in the beginning", "in the begining", "0", "top"],
+            "End": ["end", "bottom", "botom", "at end", "at bottom", "at botom", "the end", "1"],
+            "Fixed_Font": ["fixed_font", "fixed", "fix", "fixed font", "fixed_size", "fixed size", "fixed_width", "fixed width", "f"]
+        }
+        # Create list [flag, value, error]
+        commands = self._parse_flag_string(formating_flags, comm_syn)
+        # Define local formating objects
+        char_format = QtGui.QTextCharFormat(self.global_text_char_format)
+        color = QtGui.QColor(self.global_color)
+        font = QtGui.QFont(self.global_font)
+        # Set default values for variables
+        no_new_line = self.no_new_line
+        cursor = self.box.textCursor()
+        # Execute flags
+        for i in range(0, len(commands)):
+            cl = commands[i][1].lower()
+            if cl in val_syn["True"] or cl in val_syn["False"] or cl in val_syn["Start"] or cl in val_syn["End"]:
+                commands[i][1] = cl
+            if commands[i][0] in comm_syn["cls"]:
+                self.box.setText("")
+            elif commands[i][0] in comm_syn["color"]:
+                val = commands[i][1]
+                color.setNamedColor(val)
+                char_format.setForeground(color)
+            elif commands[i][0] in comm_syn["@color"]:
+                val = commands[i][1]
+                color.setNamedColor(val)
+                char_format.setForeground(color)
+                self.global_color.setNamedColor(val)
+                self.global_text_char_format.setForeground(self.global_color)
+            elif commands[i][0] in comm_syn["background"]:
+                val = commands[i][1]
+                color.setNamedColor(val)
+                char_format.setBackground(color)
+            elif commands[i][0] in comm_syn["@background"]:
+                val = commands[i][1]
+                color.setNamedColor(val)
+                char_format.setBackground(color)
+                self.global_color.setNamedColor(val)
+                self.global_text_char_format.setBackground(self.global_color)
+            elif commands[i][0] in comm_syn["font_size"]:
+                try:
+                    val = int(commands[i][1])
+                except ValueError:
+                    val = self.global_font.pointSize()
+                char_format.setFontPointSize(val)
+            elif commands[i][0] in comm_syn["@font_size"]:
+                try:
+                    val = int(commands[i][1])
+                except ValueError:
+                    val = self.global_font.pointSize()
+                char_format.setFontPointSize(val)
+                self.global_text_char_format.setFontPointSize(val)
+            elif commands[i][0] in comm_syn["font_name"]:
+                val = commands[i][1]
+                if commands[i][1] in val_syn["Fixed_Font"]:
+                    char_format.setFontFamily(QtGui.QFontDatabase.systemFont(QtGui.QFontDatabase.FixedFont).family())
+                elif commands[i][1].lower() == "roboto mono":
+                    char_format.setFontFamily(self.roboto_mono_font)
+                else:
+                    char_format.setFontFamily(val)
+            elif commands[i][0] in comm_syn["@font_name"]:
+                val = commands[i][1]
+                if commands[i][1] in val_syn["Fixed_Font"]:
+                    char_format.setFontFamily(QtGui.QFontDatabase.systemFont(QtGui.QFontDatabase.FixedFont).family())
+                    self.global_text_char_format.setFontFamily(QtGui.QFontDatabase.systemFont(QtGui.QFontDatabase.FixedFont).family())
+                elif commands[i][1].lower() == "roboto mono":
+                    char_format.setFontFamily(self.roboto_mono_font)
+                    self.global_text_char_format.setFontFamily(self.roboto_mono_font)
+                else:
+                    char_format.setFontFamily(val)
+                    self.global_text_char_format.setFontFamily(val)
+            elif commands[i][0] in comm_syn["font_bold"]:
+                val = commands[i][1]
+                if val in val_syn["True"]:
+                    font.setBold(True)
+                    char_format.setFontWeight(QtGui.QFont.Bold)
+                elif val in val_syn["False"]:
+                    font.setBold(False)
+                    char_format.setFontWeight(QtGui.QFont.Normal)
+            elif commands[i][0] in comm_syn["@font_bold"]:
+                val = commands[i][1]
+                if val in val_syn["True"]:
+                    font.setBold(True)
+                    char_format.setFontWeight(QtGui.QFont.Bold)
+                    self.global_font.setBold(True)
+                    self.global_text_char_format.setFontWeight(QtGui.QFont.Bold)
+                elif val in val_syn["False"]:
+                    font.setBold(False)
+                    char_format.setFontWeight(QtGui.QFont.Normal)
+                    self.global_font.setBold(False)
+                    self.global_text_char_format.setFontWeight(QtGui.QFont.Normal)
+            elif commands[i][0] in comm_syn["font_underline"]:
+                val = commands[i][1]
+                if val in val_syn["True"]:
+                    font.setUnderline(True)
+                    char_format.setFontUnderline(True)
+                elif val in val_syn["False"]:
+                    font.setUnderline(False)
+                    char_format.setFontUnderline(False)
+            elif commands[i][0] in comm_syn["@font_underline"]:
+                val = commands[i][1]
+                if val in val_syn["True"]:
+                    font.setUnderline(True)
+                    char_format.setFontUnderline(True)
+                    self.global_font.setUnderline(True)
+                    self.global_text_char_format.setFontUnderline(True)
+                elif val in val_syn["False"]:
+                    font.setUnderline(False)
+                    char_format.setFontUnderline(False)
+                    self.global_font.setUnderline(False)
+                    self.global_text_char_format.setFontUnderline(False)
+            elif commands[i][0] in comm_syn["new_line"]:
+                if commands[i][1] in val_syn["True"]:
+                    no_new_line = False
+                elif commands[i][1] in val_syn["False"]:
+                    no_new_line = True
+            elif commands[i][0] in comm_syn["@new_line"]:
+                if commands[i][1] in val_syn["True"]:
+                    no_new_line = False
+                    self.no_new_line = False
+                elif commands[i][1] in val_syn["False"]:
+                    no_new_line = True
+                    self.no_new_line = True
+            elif commands[i][0] in comm_syn["Move"]:
+                if commands[i][1] in val_syn["Start"]:
+                    cursor.movePosition(QtGui.QTextCursor.Start)
+                    self.box.moveCursor(QtGui.QTextCursor.Start)
+                    cursor_freeze = True
+                elif commands[i][1] in val_syn["End"]:
+                    cursor.movePosition(QtGui.QTextCursor.End)
+                    self.box.moveCursor(QtGui.QTextCursor.End)
+                    cursor_freeze = True
+        # Add text to txt_info
+        if not no_new_line:
+            txt = txt + "\n"
+        if txt.rstrip() != "`None|Ignore`":
+            cursor.insertText(txt, char_format)
+        self.box.textCursor().setPosition(cursor.position())
+        self.box.ensureCursorVisible()
+        
+    def _parse_flag_string(self, flags_string: str, flag_names_dict: dict) -> list:
+        """Handles the flags.
+        Args:
+            flags_string (str): String with user flags
+            flag_names_dict (dict): Valid flag commands
+        Returns:
+            list: List of commands, values and Errors
+        """
+        result = []
+        flag_list = flags_string.split(",")
+        for i in flag_list:
+            flag_and_value = i.split("=")
+            if len(flag_and_value) == 1:
+                result.append([flag_and_value[0].strip().lower(), "", ""])
+            elif len(flag_and_value) == 2:
+                result.append([flag_and_value[0].strip().lower(), flag_and_value[1].strip(), ""])
+            elif len(flag_and_value) > 2:
+                result.append(["", "", "The number of assigned values ​​has been exceeded"])
+            elif len(flag_and_value) == 0:
+                result.append(["", "", ""])
+
+        for idx, i in enumerate(result):
+            has_error = True
+            if i[0] != "" and i[1] != "":
+                for key, value in flag_names_dict.items():
+                    if i[0] in value:
+                        has_error = False
+                        break
+                if has_error:
+                    result[idx][2] = "Unrecognized command"
+                    result[idx][1] = flag_list[idx]
+                    result[idx][0] = ""
+        for i in result:
+            if i[2] != "":
+                self.errors.append([i[1], i[2]])
+
+        return result
+
+    def flags_has_errors(self):
+        """Returns True if the Flag string had an errors
+        """
+        if len(self.errors) > 0:
+            return True
+        else:
+            return False
+
+    def get_flag_error_list(self):
+        """List of errors found in the Flags string.
+        """
+        return self.errors        
+
+    def clear_flag_error_list(self):
+        self.errors = []
 
 
 if __name__ == "__main__":
